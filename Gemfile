ea8f36d3253ea936dbc88a236cdf2c301d60f45d
Mike Finocchiaro <pushon@onajunket.com>
2015-04-22 12:00:36 -0400

    My first heroku commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source "https://rubygems.org" #source is a method
+ruby "2.2.1"
+
+gem "sinatra"
+
+#need to test file before uploading to heroku: bundle install
+#this install and checks update
+#Procfile to run server in Procfile file
---
+++ server.rb
@@ -0,0 +1,20 @@
+require 'sinatra'
+
+get '/' do
+	"Hello World"	
+end
+
+get '/blah' do
+	"Hello World"
+end
+
+get '/sinatra' do
+	"Hello Sinatra"
+end
+
+#now we need to run the app on heroku
+# we are going to upload this file on heroku
+
+
+# separte file Gemfile
+
